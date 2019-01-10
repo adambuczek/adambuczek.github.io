98a4287f312d5ec04da88cc979c9d03e9ea42843
Adam Buczek <adambuczek@gmail.com>
2019-01-10 15:55:16 +0100

    add gulp dev and production config

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,40 +1,87 @@
 'use strict'
+require('dotenv').config()
 
-import { src, dest, parallel } from 'gulp'
+import gulp from 'gulp'
 import autoprefixer from 'gulp-autoprefixer'
-import sourcemaps from 'gulp-sourcemaps'
 import minifyCSS from 'gulp-csso'
+import minifyHTML from 'gulp-htmlmin'
+import inlineSource from 'gulp-inline-source'
 import run from 'gulp-run'
-
-const dirs = {
-    src: 'src',
-    dest: 'dist'
-}
+import browserSync from 'browser-sync'
 
 const paths = {
-    src: `${dirs.src}/styles/*.css`,
-    dest: `${dirs.dest}/styles/`
+    src: 'src',
+    dest: 'dist',
+}
+
+paths.styles = {
+    src: `${paths.src}/styles/*.css`,
+    dest: `${paths.dest}/styles/`,
+}
+
+paths.html = {
+    src: `${paths.dest}/**/*.html`,
+}
+
+paths.generate = {
+    src: [`${paths.src}/**/*.njk`, `${paths.src}/**/*.md`],
 }
 
 const styles = () => {
-    return src(paths.src)
-        .pipe(sourcemaps.init())
+    return gulp.src(paths.styles.src, { sourcemaps: true })
         .pipe(autoprefixer())
         .pipe(minifyCSS())
-        .pipe(sourcemaps.write('.'))
-        .pipe(dest(paths.dest))
+        .pipe(gulp.dest(paths.styles.dest))
+        .pipe(browserSync.stream())
+}
+
+const html = () => {
+    return gulp.src(paths.html.src)
+        .pipe(minifyHTML({ collapseWhitespace: true }))
+        .pipe(gulp.dest(file => file._base)) // minify eleventy output in place
+}
+
+const inline = () => {
+    return gulp.src(paths.html.src)
+        .pipe(inlineSource({
+            rootpath: paths.dest
+        }))
+        .pipe(gulp.dest(file => file._base)) // minify eleventy output in place
 }
 
 const generate = () => run('npx eleventy').exec()
 const clean = () => run(`rm -rf ${paths.dest}`).exec()
 
-export {
-    styles,
-    generate,
-    clean,
+const assets = gulp.parallel(styles, html)
+
+
+const reload = done => {
+    browserSync.reload()
+    done()
+}
+  
+const serve = done => {
+    browserSync.init({
+        notify: false,
+        reloadDebounce: 2000,
+        ui: false,
+        ghostMode: false,
+        browser: process.env.BROWSER || 'default',
+        server: {
+            baseDir: paths.dest,
+        }
+    })
+    done()
 }
 
-export default parallel(styles)
+const watch = () => {
+    gulp.watch(paths.styles.src, styles)
+    gulp.watch(paths.generate.src, gulp.series(generate, reload))
+}
 
 
+const build = gulp.series(clean, generate, assets, inline)
+const develop = gulp.series(clean, generate, serve, watch)
 
+export default build
+export { clean, develop }
--- src/index.md
+++ src/index.md
@@ -2,3 +2,4 @@
 layout: index.njk
 title: Index
 ---
+Dziękuję
