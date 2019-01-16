dc7f5e803467f7a6546e40711cb6074f4e3e3ff7
Adam Buczek <adambuczek@gmail.com>
2019-01-16 22:06:07 +0100

    (style) add footnotes support

--- .eleventy.js
+++ .eleventy.js
@@ -2,11 +2,21 @@ const syntaxHighlight = require('@11ty/eleventy-plugin-syntaxhighlight')
 const inclusiveLangPlugin = require('@11ty/eleventy-plugin-inclusive-language')
 const { DateTime } = require('luxon')
 
+const markdownIt = require('markdown-it')
+const mdFootnote = require('markdown-it-footnote')
+
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPlugin(syntaxHighlight)
   eleventyConfig.addPlugin(inclusiveLangPlugin)
 
+
+  // Nunjusks
   eleventyConfig.addNunjucksFilter('formatDate', dateObj => DateTime.fromJSDate(dateObj).toFormat('dd LLL yyyy'))
+
+  // Markdown  
+  eleventyConfig.setLibrary("md", markdownIt()
+    .use(mdFootnote)
+  )
 
   return {
     dir: {
